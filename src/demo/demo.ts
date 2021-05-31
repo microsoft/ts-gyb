69b6389350aeeee7f0eb0f57ef116a39f0ff7151
Zhuoran Tan <t@zhuorantan.com>
2021-05-31 20:10:52 +0800

    Generate modules

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,27 +1,20 @@
 import { Parser } from '../parser/Parser';
-import { RendererConfig, DefaultSwiftRendererConfig } from '../renderer/RenderConfig';
-import { CustomTypeCollector } from '../renderer/CustomTypeCollector';
-import { DemoCodeRenderer } from './demoCodeRenderer';
 import { fetchNamedTypes, transformModuleAndTypeName } from '../parser/named-types';
+import { SwiftModuleView } from '../renderer/ModuleView';
+import { CodeTemplateRenderer } from '../renderer/CodeTemplateRenderer';
 
 function run(): void {
-  const config = new DefaultSwiftRendererConfig();
-  console.log('Api will be generated with config: \n', JSON.stringify(config, null, 2));
-
   const parser = new Parser(['src/demo/data/demoApi.ts']);
   const apiModules = parser.parse();
   transformModuleAndTypeName(apiModules, /^I/, '');
   console.log(JSON.stringify(apiModules, null, 4));
 
-  const rendererConfig = config as RendererConfig;
-  const typeTransformer = new CustomTypeCollector(rendererConfig);
-  const renderer = new DemoCodeRenderer(rendererConfig, typeTransformer);
   const namedTypes = fetchNamedTypes(apiModules);
   console.log(JSON.stringify(namedTypes, null, 4));
 
-  renderer.print();
-
-  console.log(typeTransformer.toSourceLike().join('\n'));
+  const renderer = new CodeTemplateRenderer('templates/swift-bridge.mustache', 'generated');
+  const swiftModuleViews = apiModules.map(module => new SwiftModuleView(module));
+  renderer.renderModules(swiftModuleViews);
 }
 
 run();
---
+++ src/renderer/ModuleView.ts
@@ -0,0 +1,107 @@
+/* eslint-disable max-classes-per-file */
+
+import { BasicTypeValue, DictionaryKeyType, Field, isArraryType, isBasicType, isCustomType, isDictionaryType, isEnumType, isOptionalType, Method, Module, ValueType } from '../types';
+
+export interface MethodView {
+  readonly name: string;
+  readonly parametersDeclaration: string;
+  readonly parameters: { name: string, type: string }[];
+}
+
+export class SwiftMethodView implements MethodView {
+  constructor(
+    private method: Method,
+  ) {}
+
+  get name(): string {
+    return this.method.name;
+  }
+
+  get parametersDeclaration(): string {
+    return this.parameters.map(parameter =>  `${parameter.name}: ${parameter.type}`).join(', ');
+  }
+
+  get parameters(): { name: string, type: string }[] {
+    return this.method.parameters.map(parameter => ({ name: parameter.name, type: this.getTypeName(parameter.type) }));
+  }
+
+  private getTypeName(valueType: ValueType): string {
+    if (isBasicType(valueType)) {
+      switch (valueType.value) {
+        case BasicTypeValue.string:
+          return 'String';
+        case BasicTypeValue.number:
+          return 'Double';
+        case BasicTypeValue.boolean:
+          return 'Bool';
+        case BasicTypeValue.int:
+          return 'Int';
+        default:
+          throw Error('Type not exists');
+      }
+    }
+
+    if (isCustomType(valueType)) {
+      if (valueType.name !== undefined) {
+        return valueType.name;
+      }
+
+      // TODO: Handle literal type
+      throw Error('not handled');
+    }
+
+    if (isEnumType(valueType)) {
+      return valueType.name;
+    }
+
+    if (isArraryType(valueType)) {
+      return `[${this.getTypeName(valueType.elementType)}]`;
+    }
+
+    if (isDictionaryType(valueType)) {
+      let keyType: string;
+      switch (valueType.keyType) {
+        case DictionaryKeyType.string:
+          keyType = 'String';
+          break;
+        case DictionaryKeyType.number:
+          keyType = 'Int';
+          break;
+        default:
+            throw Error('Type not exists');
+      }
+
+      return `[${keyType}: ${this.getTypeName(valueType.valueType)}]`;
+    }
+
+    if (isOptionalType(valueType)) {
+      return `${this.getTypeName(valueType.wrappedType)}?`;
+    }
+
+    throw Error('Type not handled');
+  }
+}
+
+export interface ModuleView {
+  readonly fileName: string;
+  readonly name: string;
+  readonly methods: MethodView[];
+}
+
+export class SwiftModuleView implements ModuleView {
+  constructor(
+    private module: Module,
+  ) {}
+
+  get fileName(): string {
+    return `${this.module.name}.swift`;
+  }
+
+  get name(): string {
+    return this.module.name;
+  }
+
+  get methods(): MethodView[] {
+    return this.module.methods.map(method => new SwiftMethodView(method));
+  }
+}
