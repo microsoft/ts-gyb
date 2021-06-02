5fea01bea1588f54454693048de52748f806855e
Zhuoran Tan <t@zhuorantan.com>
2021-06-02 19:41:32 +0800

    Fix demo.ts

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -3,17 +3,15 @@ import { CodeGenerator, RenderingLanguage } from '../generator/CodeGenerator';
 function run(): void {
   const generator = new CodeGenerator();
   generator.parse({
-    tag: 'APIs',
     interfacePaths: ['src/demo/data/demoApi.ts'],
     defaultCustomTags: {},
     dropInterfaceIPrefix: true,
   });
   generator.renderModules({
-    tag: 'APIs',
+    index: 0,
     language: RenderingLanguage.Swift,
     outputDirectory: 'generated',
     moduleTemplatePath: 'templates/swift-bridge.mustache',
-    namedTypesTemplatePath: 'templates/swift-named-types.mustache',
   });
 }
 
