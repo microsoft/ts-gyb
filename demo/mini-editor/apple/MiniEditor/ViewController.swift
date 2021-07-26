02c48f9bdc579087d726fdcfa60f8d3385dc231a
Daohan Chong <dachon@microsoft.com>
2021-07-26 15:28:48 +0800

    Initialize the mini editor project

---
+++ demo/mini-editor/apple/MiniEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniEditor
+//
+//  Created by Daohan Chong on 7/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
