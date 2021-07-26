f9b7438d827e02ef989ae7be04191aaa30f7727d
Daohan Chong <dachon@microsoft.com>
2021-07-26 16:50:43 +0800

    Add platform target support

--- demo/mini-editor/web/webpack.config.ts
+++ demo/mini-editor/web/webpack.config.ts
@@ -7,62 +7,80 @@ import HTMLInlineCSSWebpackPlugin from 'html-inline-css-webpack-plugin';
 import HtmlInlineScriptPlugin from 'html-inline-script-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
-const distPath = path.resolve(__dirname, 'dist');
+const webDistPath = path.resolve(__dirname, 'dist');
 const sourcePath = path.resolve(__dirname, 'src');
 
-const config: webpack.Configuration | webpack.WebpackOptionsNormalized = {
-  mode: 'development',
-  entry: `${sourcePath}/main.ts`,
-  devServer: {
-    contentBase: distPath,
-    compress: true,
-    port: 9000,
-  },
-  output: {
-    path: distPath,
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        include: [
-          sourcePath,
-        ],
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              configFile: 'tsconfig.json',
-              // compilerOptions: {
-              //   sourceMap: true,
-              // },
-            },
-          },
-        ],
-      }
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-    new HtmlWebpackPlugin({
-      template: `${sourcePath}/index.html`,
-      filename: './bundle.html',
-      inlineSource: '.(js|css)$',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-      }
-    }),
-    new HTMLInlineCSSWebpackPlugin(),
-    new HtmlInlineScriptPlugin(),
-  ]
-};
+const appleDistPath = path.resolve(__dirname, '..', 'apple/MiniEditor/Resources');
 
-export default config;
+enum SupportedTarget {
+  web = 'web',
+  apple = 'apple',
+}
+
+interface WebpackEnvironment {
+  target: SupportedTarget;
+  port?: number;
+}
+
+function buildConfig(env: WebpackEnvironment): webpack.Configuration | webpack.WebpackOptionsNormalized {
+  const isProductionBuild = process.env.NODE_ENV === 'production';
+
+  const distPath = env.target == SupportedTarget.web ? webDistPath : appleDistPath;
+
+  return {
+    mode: isProductionBuild ? 'production' : 'development',
+    entry: `${sourcePath}/main.ts`,
+    devServer: {
+      contentBase: distPath,
+      compress: true,
+      port: env.port ?? 9000,
+    },
+    output: {
+      path: distPath,
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          include: [
+            sourcePath,
+          ],
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                configFile: 'tsconfig.json',
+                compilerOptions: {
+                  sourceMap: !isProductionBuild,
+                },
+              },
+            },
+          ],
+        }
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
+      new HtmlWebpackPlugin({
+        template: `${sourcePath}/index.html`,
+        filename: './bundle.html',
+        inlineSource: '.(js|css)$',
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+        }
+      }),
+      new HTMLInlineCSSWebpackPlugin(),
+      new HtmlInlineScriptPlugin(),
+    ]
+  };
+}
+
+export default (env) => buildConfig(env);;
