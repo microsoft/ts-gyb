64cde61bf841bfb23979ff60e20358de73474033
Daohan Chong <dachon@microsoft.com>
2021-07-28 17:14:26 +0800

    run code generation

--- demo/mini-editor/android/app/src/main/java/com/microsoft/tscodegen/demo/minieditor/generated/EditorBridge.kt
+++ demo/mini-editor/android/app/src/main/java/com/microsoft/tscodegen/demo/minieditor/generated/EditorBridge.kt
@@ -10,14 +10,6 @@ package com.microsoft.tscodegen.demo.minieditor.generated
 
 import com.google.gson.Gson
 import android.webkit.WebView
-import java.lang.reflect.Type
-import com.google.gson.JsonDeserializationContext
-import com.google.gson.JsonDeserializer
-import com.google.gson.JsonElement
-import com.google.gson.JsonPrimitive
-import com.google.gson.JsonSerializationContext
-import com.google.gson.JsonSerializer
-import com.google.gson.annotations.SerializedName
 
 interface EditorBridgeInterface {
     fun toggleBold()
