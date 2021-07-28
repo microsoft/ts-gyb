f5d84cd11b80863e587de921d5901ba58137a5d2
Daohan Chong <dachon@microsoft.com>
2021-07-28 15:40:23 +0800

    Add android gson

--- demo/mini-editor/android/app/src/main/java/com/microsoft/tscodegen/demo/minieditor/MainActivity.kt
+++ demo/mini-editor/android/app/src/main/java/com/microsoft/tscodegen/demo/minieditor/MainActivity.kt
@@ -2,13 +2,20 @@ package com.microsoft.tscodegen.demo.minieditor
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Base64
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.microsoft.tscodegen.demo.minieditor.generated.EditorBridge
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.util.*
+import kotlin.concurrent.schedule
 
 private const val BUNDLE_FILENAME = "bundle.html"
 
@@ -31,6 +38,15 @@ class MainActivity : AppCompatActivity() {
         // Adjust from the official example: https://developer.android.com/guide/webapps/webview
         val encodedHtml = Base64.encodeToString(htmlString.toByteArray(), Base64.NO_PADDING)
         webView.loadData(encodedHtml, "text/html", "base64")
+
+        val gson = GsonBuilder().create()
+        val bridge = EditorBridge(webView, gson)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            bridge.insertContent("test", true) { result ->
+
+            }
+        }, 1500)
     }
 
     private fun convertStreamToString(inputStream: InputStream): String? {
