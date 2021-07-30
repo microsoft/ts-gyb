2613108086383550e775ef60e318b4c415d38ee9
Zhuoran <t@zhuorantan.com>
2021-07-30 20:42:20 +0800

    Skip generating shared types when not exist (#60)

--- demo/basic/generated/swift/SharedTypes.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Copyright (c) Microsoft Corporation. All rights reserved.
-//
-
-// swiftformat:disable redundantRawValues
-// Don't modify this file manually, it's auto generated.
-
-import UIKit
