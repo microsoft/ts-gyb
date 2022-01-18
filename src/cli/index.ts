2e4f93d597bc2aa75e3c1e485e11d567d3efa5c2
Zhuoran <t@zhuorantan.com>
2022-01-18 16:06:35 +0800

    Add a command to list all output files (#88)

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,18 +1,34 @@
 import path from 'path';
+import fs from 'fs';
 import yargs from 'yargs';
 import { CodeGenerator, RenderingLanguage } from '../generator/CodeGenerator';
-import { Configuration, normalizeConfiguration } from './configuration';
+import { Configuration, normalizeConfiguration, RenderConfiguration } from './configuration';
 
 const program = yargs(process.argv.slice(2));
 
-const args = program.config().help().argv;
-
 function run(): void {
-  const configPath = args.config as string;
-  const projectDirectory = path.resolve(path.dirname(configPath));
-  process.chdir(projectDirectory);
+  /* eslint-disable no-empty-function,@typescript-eslint/no-empty-function */
+  program
+    .scriptName('ts-gyb')
+    .command(['gen', '*'], 'generate code from a configuration file', () => {}, generate)
+    .command('list-output', 'list all output files', (subprogram) => {
+      subprogram
+        .option('language', { description: 'language of the output files to list', choices: ['swift', 'kotlin'] })
+        .option('expand', { description: 'expand directories' });
+    }, listOutput)
+    .option('config', {
+      describe: 'path to the configuration file',
+      type: 'string',
+    })
+    .demandCommand(1, 'You need at least one command before moving on')
+    .demandOption('config')
+    .help()
+    .parse();
+  /* eslint-enable no-empty-function,@typescript-eslint/no-empty-function */
+}
 
-  const config = normalizeConfiguration(args as unknown as Configuration, projectDirectory);
+function generate(args: { config: string }): void {
+  const config = parseConfig(args.config);
 
   const generator = new CodeGenerator();
   Object.entries(config.parsing.source).forEach(([tag, interfacePaths]) => {
@@ -60,6 +76,45 @@ function run(): void {
       typeNameMap: renderingConfig.typeNameMap ?? {},
     });
   });
+}
+
+function listOutput(args: { config: string; language?: 'swift' | 'kotlin'; expand: boolean }): void {
+  const config = parseConfig(args.config);
+
+  let files: string[];
+  if (args.language !== undefined) {
+    const renderingConfig = config.rendering[args.language];
+    if (renderingConfig === undefined) {
+      throw new Error(`Language ${args.language} is not defined in the configuration file`);
+    }
+    files = Object.values(renderingConfig.outputPath);
+  } else {
+    files = Object.values(config.rendering).map((renderingConfig: RenderConfiguration) => Object.values(renderingConfig.outputPath)).flat();
+  }
+
+  files = files.map((file) => path.resolve(file));
+  if (args.expand) {
+    files = files.map((filePath) => {
+      if (!fs.lstatSync(filePath).isDirectory()) {
+        return filePath;
+      }
+
+      return fs.readdirSync(filePath).map((file) => path.join(filePath, file));
+    }).flat();
+  }
+
+  files = [...new Set(files)];
+  console.log(files.join('\n'));
+}
+
+function parseConfig(configPath: string): Configuration {
+  const configFile = path.resolve(configPath);
+  const projectDirectory = path.dirname(configFile);
+
+  process.chdir(projectDirectory);
+
+  const rawConfig = JSON.parse(fs.readFileSync(configFile).toString()) as Configuration;
+  return normalizeConfiguration(rawConfig, projectDirectory);
 }
 
 run();
