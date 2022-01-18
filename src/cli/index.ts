6009a1aa11d325ca9290ada9255744290dd28d09
Zhuoran Tan <t@zhuorantan.com>
2022-01-18 15:10:39 +0800

    Add a command to list all sources

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,18 +1,35 @@
 import path from 'path';
+import fs from 'fs';
 import yargs from 'yargs';
+import { glob } from 'glob';
 import { CodeGenerator, RenderingLanguage } from '../generator/CodeGenerator';
 import { Configuration, normalizeConfiguration } from './configuration';
 
 const program = yargs(process.argv.slice(2));
 
-const args = program.config().help().argv;
-
 function run(): void {
-  const configPath = args.config as string;
-  const projectDirectory = path.resolve(path.dirname(configPath));
-  process.chdir(projectDirectory);
+  /* eslint-disable no-empty-function,@typescript-eslint/no-empty-function */
+  program
+    .scriptName('ts-gyb')
+    .command('gen', 'generate code from a configuration file', () => {}, generate)
+    .command('list-sources', 'list all available source files', (subprogram) => {
+      subprogram
+        .option('tag <tag>', { description: 'tag of the source files to list', type: 'string' })
+        .option('expand', { description: 'expand globstar' });
+    }, listSources)
+    .option('config', {
+      describe: 'path to the configuration file',
+      type: 'string',
+    })
+    .demandCommand(1, 'You need at least one command before moving on')
+    .demandOption('config')
+    .help()
+    .parse();
+  /* eslint-enable no-empty-function,@typescript-eslint/no-empty-function */
+}
 
-  const config = normalizeConfiguration(args as unknown as Configuration, projectDirectory);
+function generate(args: { config: string }): void {
+  const config = parseConfig(args.config);
 
   const generator = new CodeGenerator();
   Object.entries(config.parsing.source).forEach(([tag, interfacePaths]) => {
@@ -60,6 +77,40 @@ function run(): void {
       typeNameMap: renderingConfig.typeNameMap ?? {},
     });
   });
+}
+
+function listSources(args: { config: string; tag?: string; expand: boolean }): void {
+  const config = parseConfig(args.config);
+
+  let files: string[];
+  if (args.tag !== undefined) {
+    if (config.parsing.source[args.tag] === undefined) {
+      throw new Error(`Tag ${args.tag} is not defined in the configuration file`);
+    }
+    files = config.parsing.source[args.tag];
+  } else {
+    files = Object.values(config.parsing.source).flat();
+  }
+
+  files = files.map((file) => path.resolve(file));
+
+  if (args.expand) {
+    files = files.flatMap((pattern) => glob.sync(pattern));
+  }
+
+  files = [...new Set(files)];
+
+  console.log(files.join('\n'));
+}
+
+function parseConfig(configPath: string): Configuration {
+  const configFile = path.resolve(configPath);
+  const projectDirectory = path.dirname(configFile);
+
+  process.chdir(projectDirectory);
+
+  const rawConfig = JSON.parse(fs.readFileSync(configFile).toString()) as Configuration;
+  return normalizeConfiguration(rawConfig, projectDirectory);
 }
 
 run();
